e9365f6e12d354fd9f87d797011615feaa7be129
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2021-02-22 21:31:00 +0100

    cleaned task styling, added important style

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 
@@ -6,14 +6,22 @@ import Typography from "@material-ui/core/Typography";
 
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-export default function Task ({task, id, taskTekst, taskNotes, taskImportance, deleteTask, completeTask, showEditModal, editTask}) {
-
-
+export default function Task({
+  task,
+  id,
+  taskTekst,
+  taskNotes,
+  taskImportance,
+  deleteTask,
+  completeTask,
+  showEditModal,
+  editTask,
+}) {
   //TASK STYLE////////
   const useStyles = makeStyles({
     //card styles
@@ -21,15 +29,14 @@ export default function Task ({task, id, taskTekst, taskNotes, taskImportance, d
       marginTop: ".5rem",
       marginBottom: ".5rem",
       transition: "all .2s",
-      
+      borderLeft: taskImportance ? "10px solid gold" : "10px solid transparent",
+
+      backgroundColor: task.completed ? "grey" : "",
+      textDecoration: task.completed ? "line-through" : "",
     },
 
-    taskCompleted: {
-      backgroundColor: "grey",
-    },
-
-    taskImportant: {
-      backgroundColor: "gold !important",
+    checkbox: {
+      marginLeft: "-100px",
     },
 
     //text styles
@@ -39,74 +46,77 @@ export default function Task ({task, id, taskTekst, taskNotes, taskImportance, d
     },
 
     deleteButton: {
-    marginLeft: "auto !important"
-    }
+      marginLeft: "auto !important",
+    },
   });
 
-  const classes = useStyles();
+  const classes = useStyles(taskImportance, task.completed);
 
   //TASK INPUT  HANDLING///////////////
   const [checked, setChecked] = React.useState(false);
   const handleChange = (event) => {
     setChecked(event.target.checked);
-   
-    setTimeout (() => {completeTask(task.id)}, 1000);
+
+    setTimeout(() => {
+      completeTask(task.id);
+    });
   };
 
-//TASK EDITING//////////////////
-const handleEditing = (id) => {
-  editTask(id)
-  showEditModal();
+  //TASK EDITING//////////////////
+  const handleEditing = (id) => {
+    editTask(id);
+    showEditModal();
+  };
 
-}
+  useEffect(() => {
+    setChecked(task.completed);
+  });
 
-useEffect(() => {
-  setChecked(task.completed)
-  
-}, )
-
-
-const checkTaskStatusStyling = () =>{
-  if(taskImportance){
-    return classes.taskImportant
-  }
-  if(task.completed){
-    return classes.taskCompleted
-  }
-
-}
+  const checkTaskStatusStyling = () => {
+    if (taskImportance) {
+      return classes.taskImportant;
+    }
+    if (task.completed) {
+      return classes.taskCompleted;
+    }
+  };
 
   return (
     <Card
       variant="outlined"
       key={id}
-      className={ `${classes.root} ${checkTaskStatusStyling()}`}
+      className={`${classes.root} ${checkTaskStatusStyling()}`}
     >
       <CardActions>
         <Checkbox
           checked={checked}
           onChange={handleChange}
           inputProps={{ "aria-label": "primary checkbox" }}
+          classes={classes.checkbox}
         />
-        <Typography
-          variant="body1"
-          className={
-            task.completed ? classes.taskTextChecked : classes.taskTextUnchecked
-          }
-        >
+        <Typography variant="body1">
           <strong>{taskTekst}</strong>
           <br></br>
           {taskNotes}
         </Typography>
-        <IconButton className={classes.deleteButton} onClick={() => {handleEditing(task.id)}} aria-label="delete" >
+        <IconButton
+          className={classes.deleteButton}
+          onClick={() => {
+            handleEditing(task.id);
+          }}
+          aria-label="delete"
+        >
           <EditIcon />
         </IconButton>
-        <IconButton onClick={() => {deleteTask(task.id)}} aria-label="delete" >
-          <DeleteIcon  />
+        <IconButton
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+          aria-label="delete"
+        >
+          <DeleteIcon />
         </IconButton>
       </CardActions>
     </Card>
   );
-};
-
-
+}
